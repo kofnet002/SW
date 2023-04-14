c1b43b70811ee907f4205d498551fb81d94866dc
frimpong-kofi <noeljoel61@gmail.com>
2023-04-14 03:22:47 +0000

    Initial commit with models

---
+++ sw/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None):
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+
+        user = self.model(phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password):
+        user = self.create_user(phone_number=phone_number, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    full_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=20, unique=True)
+    location = models.CharField(max_length=255)
+    national_id = models.CharField(max_length=20, unique=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True, default="net.jpg")
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+class Client(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='client')
+    occupation = models.CharField(max_length=50)
+
+    # def __str__(self):
+    #     return user.__username
+
+class Worker(models.Model):
+    SKILLS = (
+        ('plumber', 'Plumber'),
+        ('electrician', 'Electrician'),
+        ('carpenter', 'Carpenter'),
+        # Add more skills as needed
+    )
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='worker')
+    skill = models.CharField(max_length=50, choices=SKILLS)
+
+    def __str__(self):
+        return user
---
+++ sw/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'full_name', 'age', 'gender', 'phone_number', 'location', 'national_id', 'profile_picture', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data['full_name'],
+            age=validated_data['age'],
+            gender=validated_data['gender'],
+            phone_number=validated_data['phone_number'],
+            location=validated_data['location'],
+            national_id=validated_data['national_id'],
+            profile_picture=validated_data['profile_picture'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Client
+        fields = ['id', 'user', 'occupation']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        client = Client.objects.create(user=user, occupation=validated_data['occupation'])
+        return client
+
+
+class WorkerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Worker
+        fields = ['id', 'user', 'skill']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        worker = Worker.objects.create(user=user, skill=validated_data['skill'])
+        return worker
